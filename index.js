8f9339658a499d13aa4ad39ca90da5970c3bd5e9
Volodymyr bc2024-5 <v8887753@gmail.com>
2025-11-04 17:27:10 +0200

    Part 1

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
 import { Command } from 'commander';
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
 
 const program = new Command();
 
-program.option('-i, --input <path>');
+program
+  .requiredOption('-h, --host <address>', 'server host')
+  .requiredOption('-p, --port <number>', 'server port')
+  .requiredOption('-c, --cache <dir>', 'cache directory');
 
 program.parse(process.argv);
 const options = program.opts();
 
-console.log("Input file:", options.input);
+// --- Перевірка і створення директорії кешу ---
+if (!fs.existsSync(options.cache)) {
+  fs.mkdirSync(options.cache, { recursive: true });
+  console.log(`Cache directory created at ${options.cache}`);
+}
+
+// --- Створюємо сервер ---
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello, server is running!\n');
+});
+
+server.listen(parseInt(options.port), options.host, () => {
+  console.log(`Server running at http://${options.host}:${options.port}/`);
+});
